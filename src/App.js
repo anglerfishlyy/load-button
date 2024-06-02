f63f1aa91f317f37836d35d92a4c46c1498925be
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-06-02 15:27:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoadMoreData from './index.jsx'; // Import LoadMoreData from index.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMoreData /> {/* Render LoadMoreData component here */}
+    </div>
+  );
+}
+
+export default App;
